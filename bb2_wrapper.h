0e90eb8eadbdfe0200084e9a65ba80d7f1388d52
Highlight Clark <highlightz@163.com>
2015-09-05 08:52:55 +0800

    Update bb2_wrapper.h

--- bb2_wrapper.h
+++ bb2_wrapper.h
@@ -2,11 +2,14 @@
 #define BB2_WRAPPER_H
 
 #include "StereoCamera.h"
-#include <fstream>
-#include <vector>
 
+#include <fstream>
 using std::ofstream;
+
+#include <vector>
 using std::vector;
+
+#include <opencv2/opencv.hpp>
 using cv::Mat;
 
 // The point cloud refers to camera as a reference coordinate frame.
@@ -20,57 +23,84 @@ struct PointCloud
 // Such a struct object represents a neighborhood of an interest point,
 // which serves to determine the ground plane.
 // Three of them are needed.
-struct EffectivePoint
+struct DepthWindow
 {
-	float avrgDepth;
-	int numOfEffectivePoints;  // Number of effective point clouds in this area.
-	
-	// Number of near point clouds, used to determine a threshold,
-	// which tells the car there is danger in that direction.
-	int numPixelLessThanFixedDepthThreshold;   
+	float avrgDepth;  // Average depth of a window
+	int numOfPoints;  // Number of points of this window
+	int numPixelLessThanFixedDepthThreshold;  // Number of points with depth less than a fixed value
 };
 
 class bb2_wrapper : public CstereoCamera
 {
 public:
+	// ---------------------------------------
+	// Construction / Deconstruction
 	bb2_wrapper( int sw = 320, int sh = 240 );
 	~bb2_wrapper( );
-
-	void setDispRange( int minDisp, int maxDisp );
-
-	bool StartCamera( );
-
-	// Show camera info
-	void showCameraInfo( );
-
-	Mat getDepthImage16( ) const;
-
-	// Only for display
-	IplImage* getStereo( ) const;
-
-	PointCloud getPointCloud( ) const;
-
-	bool StereoMatch( ); 
-
-	void showInterestPointsDepth( ) const;
-
-	void showInterestPoints3D( ) const;
-
-	void showAvrgDepth( );
+	// ---------------------------------------
 	
+	// -------------------------------------------------------------------------------------------
+	// Stereo camera setting and control
+	void setDispRange( int minDisp, int maxDisp );  // Sets the disparity range of stereo matching
+	bool StartCamera( );
+	void showCameraInfo( );  // Print stereo camera intrinsics
+	// -------------------------------------------------------------------------------------------
+
+	// -----------------------------------------------------------
+	// Stereo matching method, most important method of this class
+	bool StereoMatch( ); 
+	// -----------------------------------------------------------
+
+	// ------------------------------------------------------------------------------------------------------
+	// Data acquisition
+	Mat getDepthImage16( ) const;  // 16-bit disparity image
+	IplImage* getStereo( ) const;  // 8-bit disparity image, easy to display
+	PointCloud getPointCloud( ) const;  // A point of frame point clouds collection
+	vector< CvPoint3D32f > getNinePoints( ) const;  // Nine sampled points from frame point clouds collection
+	vector< PointCloud > getFramePointClouds( ) const;  // Frame point clouds collection
+	// ------------------------------------------------------------------------------------------------------
+
+	// ------------------------------------------------------------------------------------------------------
+	// Data display
+	void showFifteenPointsDepth( ) const;  // Fifteen sampled points from point clouds, containing depth info
+	void showFifteenPoints3D( ) const;  // Fifteen sampled points from point clouds, containing 3d info
+	// ------------------------------------------------------------------------------------------------------
+
+	// Others
+	void showAvrgDepth( );
+
 	void showSampledGround( );
 
-	vector< CvPoint3D32f >  getNinePoints( ) const;
-	
 	void useGround( );
-	
-	cv::Mat groundImg;  // Stores the ground plane info
+
+	cv::Mat groundImg;
 private:
+	// ---------------------------
+	// Stereo matching parameters
 	int minDisparity;
 	int maxDisparity;
-	PointCloud pc;
-	
-	struct Plane3Points
+	// ---------------------------
+
+	// ------------------------------------------------------------
+	// Data acquisition
+	PointCloud pc;  // Point cloud data
+	vector< PointCloud > pc_seq;  // Point clouds of a whole frame
+	void bufferFramePointClouds( );
+	// Sample 15 points from point clouds, containing depth info
+	vector< float > fifteenPointsDepth;
+	void bufferFifteenPointsDepth( );
+	// Sample 15 points from point clouds, containing 3d info
+	vector < CvPoint3D32f > fifteenPoints3D;
+	void bufferFifteenPoints3D( );
+	// Sample 9 points form point clouds
+	vector< CvPoint3D32f > ninePoints;
+	void sampleNinePoints( );
+	// ------------------------------------------------------------
+
+	// ------------------------------------------------------
+	// Derived data
+	// Sample three areas of points to compute a ground plane 
+	struct GroundPlanePoints
 	{
 		CvPoint3D32f planePointA;
 		int numOfEffectivePointsAourndA;
@@ -80,58 +110,24 @@ private:
 
 		CvPoint3D32f planePointC;
 		int numOfEffectivePointsAourndC;
-		
-		Plane3Points( )
-		{
-			planePointA.x = 0;
-			planePointA.y = 0;
-			planePointA.z = 0;
-			numOfEffectivePointsAourndA = 0;
+	} groundPlanePoints;
+	void sampleGroundPoints( );
 
-			planePointB.x = 0;
-			planePointB.y = 0;
-			planePointB.z = 0;
-			numOfEffectivePointsAourndB = 0;
-
-			planePointC.x = 0;
-			planePointC.y = 0;
-			planePointC.z = 0;
-			numOfEffectivePointsAourndC = 0;
-		}
-	} ground;
-	
-	// Stores the params of a plane equation
-	struct FinalGround
+	// Ground plane computed from three fixed "points",
+	// specified by prior experience.
+	// Such a plane is determined by four parameters, namely,
+	// coefs_0 * x + coefs_1 * y + coefs_2 * z + coefs_3 = 0.
+	struct GroundPlane
 	{
 		float coefs_0;
 		float coefs_1;
 		float coefs_2;
 		float coefs_3;
-		FinalGround( )
-		{
-			coefs_0 = 0.0f;
-			coefs_1 = 0.0f;
-			coefs_2 = 0.0f;
-			coefs_3 = 0.0f;
-		}
-	} finalGroundPlane;
-	
-	void sampleGround( );
-	
-	// Working mode: batch processing 
-	void bufferGround( );
-	
-	vector< float > fifteenPointsDepth;
-	void bufferFifteenPointsDepth( ); 
+	} groundPlane;
+	void genGroundPlane( );
 
-	vector < CvPoint3D32f > fifteenPoints3D;
-	void bufferFifteenPoints3D( );
-
-	vector< EffectivePoint > eP;
+	vector< DepthWindow > eP;
 	void computeAvrgDepth( );
-
-	vector< CvPoint3D32f > ninePoints;
-	void sampleNinePoints( );
+	// ------------------------------------------------------
 };
-
 #endif
